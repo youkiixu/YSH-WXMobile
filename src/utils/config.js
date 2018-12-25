6f359a64ad96d6319e4294197fefedba57ea3ea1
MinGYu <lllidan120@163.com>
2018-12-25 14:26:26 +0800

    合并印讯代码

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ let ApiKiyUrl = 'https://www.kiy.cn/'
 // ApiKiyUrl = 'http://localhost:7634/'
 // ApiKiyUrl = 'http://192.168.0.91:8008/'
 // ApiKiyUrl = 'http://192.168.30.6:6348/'
-ApiKiyUrl = 'http://yx15.kiy.cn/'
+// ApiKiyUrl = 'http://yx15.kiy.cn/'
 
 let isYinXun = ApiKiyUrl != 'https://www.kiy.cn/'
 module.exports = {
